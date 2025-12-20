cf00a674a8dd2a88191c925172cdeabd823b921a
Terminal07 <colterhardy@gmail.com>
2025-12-19 20:49:00 -0600

    rerouted oscillator stuff - still need to finish i2c routing, also started a platformio board definition

---
+++ firmware/mpu/board_def/variant.h
@@ -0,0 +1,10 @@
+#ifndef _VARIANT_ARDUINO_STM32_
+#define _VARIANT_ARDUINO_STM32_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// Pin defs
+#define PA0 // todo
+// good resource: https://community.platformio.org/t/use-arduino-framework-for-a-custom-stm32-board/14586/4
